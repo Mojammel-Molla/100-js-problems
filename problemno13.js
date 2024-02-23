24665da2be0cec500204e90c4c112d225df58f4b
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-23 23:25:42 +0600

    Problem number 13 has been solved

---
+++ problemno13.js
@@ -0,0 +1,11 @@
+// Write a function that calculates the factorial of a given non-negative integer.
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(5));
