33a12d30c21abb12c70d45712616a8f0e451d3dc
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-03-07 23:26:59 +0600

    Problem no 16 has been solved

---
+++ problemno16.js
@@ -0,0 +1,12 @@
+// Write a function to flatten a nested array.
+function flattenArray(arr) {
+  return arr.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val),
+    []
+  );
+}
+
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const flattenedArray = flattenArray(nestedArray);
+console.log(flattenedArray);
