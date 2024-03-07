31ccf447b5e4764f69bab9c6b43271bddcb8dffe
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-03-07 23:32:30 +0600

    Problem no 19 has been solved

---
+++ probllemno19.js
@@ -0,0 +1,17 @@
+// Write a function to find the maximum sum of a contiguous subarray within an array of numbers.
+
+function maxSubarraySum(arr) {
+  let maxSum = arr[0];
+  let currentSum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(arr[i], currentSum + arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const maxSum = maxSubarraySum(array);
+console.log(maxSum);
